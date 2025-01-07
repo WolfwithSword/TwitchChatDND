8bb905ff6c5641dda82fe992b181ea33008d2130
WolfwithSword <wolfwithsword4426@gmail.com>
2025-01-07 19:51:15 -0400

    Fix resources file lookup (#11)
    * Change resources structure for build
    
    * Fix build
    
    * Add hidden import for build
    
    * Fix resources file lookup

--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -4,6 +4,9 @@ def get_resource_path(relative_path):
     # Get absolute path to a resource, frozen or local (relative to helpers/utils.py)
     if getattr(sys, 'frozen', False):
         base_path = sys._MEIPASS
+        if not os.path.isfile(os.path.join(base_path, relative_path)):
+            if not os.path.isdir(os.path.join(base_path, relative_path)):
+                base_path = os.path.dirname(os.path.abspath(__file__))
     else:
         base_path = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(base_path, relative_path)
