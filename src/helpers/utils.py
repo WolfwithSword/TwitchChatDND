a1828abed2f0a1f443ec3e4a2807637c700f2d6d
WolfwithSword <wolfwithsword4426@gmail.com>
2025-01-07 18:59:18 -0400

    Fix for nightly build virus detection (#5)
    * Update build.yml
    
    * Update build.yml
    
    * Add nightly release
    
    * Update build.yml
    
    * Update docstrings for build

--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -1,7 +1,7 @@
 import os, sys
 
 def get_resource_path(relative_path):
-    """Get absolute path to a resource, frozen or local (relative to helpers/utils.py)"""
+    # Get absolute path to a resource, frozen or local (relative to helpers/utils.py)
     if getattr(sys, 'frozen', False):
         base_path = sys._MEIPASS
     else:
