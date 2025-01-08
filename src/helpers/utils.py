d8700235f74e074fd87887ba56a54233a5010b08
WolfwithSword <12175651+WolfwithSword@users.noreply.github.com>
2025-01-07 20:12:09 -0400

    Fix for resources in build

--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -1,12 +1,15 @@
 import os, sys
 
-def get_resource_path(relative_path):
+def get_resource_path(relative_path, from_resources: bool):
     # Get absolute path to a resource, frozen or local (relative to helpers/utils.py)
     if getattr(sys, 'frozen', False):
         base_path = sys._MEIPASS
-        if not os.path.isfile(os.path.join(base_path, relative_path)):
-            if not os.path.isdir(os.path.join(base_path, relative_path)):
-                base_path = os.path.dirname(os.path.abspath(__file__))
+        if from_resources:
+            base_path = os.path.join(os.path.abspath("."), "resources")
+        else:
+            if not os.path.isfile(os.path.join(base_path, relative_path)):
+                if not os.path.isdir(os.path.join(base_path, relative_path)):
+                    base_path = os.path.dirname(os.path.abspath(__file__))
     else:
         base_path = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(base_path, relative_path)
