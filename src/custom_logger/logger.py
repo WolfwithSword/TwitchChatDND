10aac7a0136341423b12c2b8f169cd672e27fb22
WolfwithSword <wolfwithsword4426@gmail.com>
2025-01-08 12:56:45 -0400

    Async logger and app teardown (#14)

--- src/custom_logger/logger.py
+++ src/custom_logger/logger.py
@@ -1,13 +1,67 @@
 import logging
+import os
+import sys
+import asyncio
+from logging.handlers import RotatingFileHandler, QueueHandler, QueueListener
+from queue import Queue
 
-class CustomHandler(logging.StreamHandler):
+_format = "%(asctime)s [%(threadName)s] [%(name)s] [%(module)s] [%(levelname)s] - %(message)s"
 
+class CustomStreamHandler(logging.StreamHandler):
     def __init__(self):
-        logging.StreamHandler.__init__(self)
-        formatter = logging.Formatter('%(asctime)s [%(threadName)s] [%(name)s] [%(module)s] [%(levelname)s] - %(message)s')
+        super().__init__()
+        formatter = logging.Formatter(_format)
         self.setFormatter(formatter)
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG) # TODO: Env var
-logger.addHandler(CustomHandler())
+class CustomFileHandler(RotatingFileHandler):
+    def __init__(self, log_file):
+        super().__init__(
+            log_file,
+            maxBytes=10 * 1024 * 1024,  # 10 MB
+            backupCount=5,
+            encoding='utf-8'
+        )
+        formatter = logging.Formatter(_format)
+        self.setFormatter(formatter)
 
+class CustomLogger:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+        debug_mode = os.environ['TCDND_DEBUG_MODE'] == '1'
+        self.logger.setLevel(logging.DEBUG if debug_mode else logging.INFO)  # TODO env var
+
+        self.log_queue = Queue()
+
+        is_frozen = getattr(sys, 'frozen', False)
+
+        if is_frozen:
+            # if getattr(sys, '_MEIPASS', False):
+            #     base_path = sys._MEIPASS
+            # else:
+            base_path = os.path.dirname(sys.executable)
+            log_folder = os.path.join(base_path, 'logs')
+        else:
+            log_folder = os.path.join(os.getcwd(), 'logs')
+
+        os.makedirs(log_folder, exist_ok=True)
+        log_file = os.path.join(log_folder, 'app.log')
+
+        if is_frozen:
+            file_handler = CustomFileHandler(log_file)
+            handlers = [file_handler]
+        else:
+            stream_handler = CustomStreamHandler()
+            handlers = [stream_handler]
+
+        self.listener = QueueListener(self.log_queue, *handlers, respect_handler_level=True)
+        self.listener.start()
+
+        queue_handler = QueueHandler(self.log_queue)
+        self.logger.addHandler(queue_handler)
+
+        self.logger.propagate = False
+
+    def shutdown(self):
+        self.listener.stop()
+
+logger = CustomLogger("ChatDND").logger
