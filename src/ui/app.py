b88aa8b7f555427c152c95ea3b832a24880fed52
WolfwithSword <12175651+WolfwithSword@users.noreply.github.com>
2025-03-10 09:11:05 -0300

    Fix floating notif colours

--- src/ui/app.py
+++ src/ui/app.py
@@ -75,7 +75,7 @@ class DesktopApp(ctk.CTk):
         self.running = False
         self.destroy()
 
-    def _show_floating_notif(self, text: str, _type: NotifyType, data: dict = None):
+    def _show_floating_notif(self, text: str, _type: NotifyType, data: dict = {}):
         if text and _type:
             if data is not None and isinstance(data, dict):
                 data.setdefault("bg_color", "#202020")
